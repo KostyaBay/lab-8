98af33a047844b9103e8cfd2cd6eb0a24a706644
kostya_bay <54154093+KostyaBay@users.noreply.github.com>
2019-12-13 01:30:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,54 @@
 #include "Header.h"
 #include "Header.cpp"
 
-int main()
+  int main()
 {
-    my_text* first;
-    cin>>T;
-    cout<<"Введите количество строк:"<<T;
-    input(first=nullptr,T);
-    output(first);
+  int k;
+  int T;
+  cin>>T; 
+  my_text* first;
+  my_text* top = first;
+    first = new my_text;
+     while(T!=0)
+    {
+      cout<<"Введите строку:"<<" ";
+    cin>>first->str;
+    first->next=new my_text; 
+    first=first->next;
+    T--;
+    }
+      first->next=nullptr;
+    /*  while(top!=nullptr)
+    {
+        cout<<top->str;
+        top=top->next;
+    }*/
+
+
+    cout<<"Введите количество строк:";
+    //input( first);
+    //output(first);
+    //cout<<"hgfhgf";
+    cin>>k;
+    switch (k){
+    case 1:
     count(T);
-    elem(T,first);
+    break;
+    case 2:
     transpotion(first);
+    break;
+    case 3:
     change(first);
+    break;
+    case 4:
     enter(T);
-    vvod();
+    break;
+    case 5:
+    vvod(T);
+    break;
+    }
+    
+    
+    
     return 0;
 }
